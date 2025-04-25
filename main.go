4ff86ee6b490a01274e7151a60b551bbb201a949
taran1sdev <thomasairddev@gmail.com>
2025-04-25 19:41:50 +0100

    Added optional parameter to search faq endpoing by id

--- main.go
+++ main.go
@@ -19,13 +19,50 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ</h1>
-	<ul>
-		<li>Is there a free version? Yes, there is!</li>
-		<li>What are your support hours? 24/7!</li>
-		<li>How do I contact support? Send an email to <a href="mailto:support@site.com">Support</a>.</li>
-	</ul>
+
+	fmt.Fprint(w, `
+		<h1>FAQ</h1>
+
+		<ul>
+			<li>Is there a free version? <strong>Yes, there is!</strong></li>
+			<li>What are your support hours? 24/7!</li>
+			<li>How do I contact support? Send an email to <a href="mailto:support@site.com">Support</a>.</li>
+		</ul>
 	`)
+}
+
+func faqIdHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	questionID := chi.URLParam(r, "QID")
+	printStr := `<h1>FAQ</h1>`
+
+	switch questionID {
+	case "1":
+		printStr += `
+		<ul>
+			<li>Is there a free version? Yes, there is!</li>
+		</ul>`
+	case "2":
+		printStr += `
+		<ul>
+			<li>What are your support hours? <strong>24/7!</strong></li>
+		</ul>`
+	case "3":
+		printStr += `
+		<ul>
+			<li>How do I contact support? <strong>Send an email to <a href="mailto:support@site.com">Support</a></strong></li>
+		</ul>`
+	default:
+		printStr += `
+		<ul>
+			<li>Is there a free version? <strong>Yes, there is!</strong></li>
+			<li>What are your support hours? 24/7!</li>
+			<li>How do I contact support? Send an email to <a href="mailto:support@site.com">Support</a>.</li>
+		</ul>`
+	}
+
+	fmt.Fprint(w, printStr)
 }
 
 func main() {
@@ -33,7 +70,9 @@ func main() {
 
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
+
 	r.Get("/faq", faqHandler)
+	r.Get("/faq/{QID}", faqIdHandler)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
